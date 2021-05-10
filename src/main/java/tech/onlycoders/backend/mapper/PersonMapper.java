e987c388910a81e7651301a5ce0c207454a50824
Juan M <43254410+juan5028@users.noreply.github.com>
2021-05-10 13:40:19 -0300

    feat: endpoint profile (#12)

--- src/main/java/tech/onlycoders/backend/mapper/PersonMapper.java
+++ src/main/java/tech/onlycoders/backend/mapper/PersonMapper.java
@@ -2,10 +2,12 @@ package tech.onlycoders.backend.mapper;
 
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import tech.onlycoders.backend.dto.person.response.ReadPersonDto;
 import tech.onlycoders.backend.dto.person.response.ReadPersonLiteDto;
 import tech.onlycoders.backend.model.Person;
 
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface PersonMapper {
   ReadPersonLiteDto personToReadPersonLiteDto(Person person);
+  ReadPersonDto personToReadPersonDto(Person person);
 }
