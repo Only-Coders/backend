ec18962d4518d2e61b301c9706e4aa45ea9f0b6e
Mariano Z <marianoz@zoho.com>
2021-05-17 18:54:31 -0300

    fix: return data from ReadWorkPositionDto

--- src/main/java/tech/onlycoders/backend/dto/workposition/response/ReadWorkPositionDto.java
+++ src/main/java/tech/onlycoders/backend/dto/workposition/response/ReadWorkPositionDto.java
@@ -1,3 +1,28 @@
 package tech.onlycoders.backend.dto.workposition.response;
 
-public class ReadWorkPositionDto {}
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.neo4j.core.schema.Relationship;
+import org.springframework.data.neo4j.core.support.DateLong;
+import tech.onlycoders.backend.model.Workplace;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReadWorkPositionDto {
+
+  @Relationship(type = "ON", direction = Relationship.Direction.OUTGOING)
+  private Workplace workplace;
+
+  @DateLong
+  private Date since;
+
+  @DateLong
+  private Date until;
+
+  private String position;
+}
