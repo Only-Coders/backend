cfe464365bce36903594830dc929f2daf702c553
Martin Navarrete <martinnp@live.com>
2021-05-20 18:52:08 -0300

    fix: get tags (#34)

--- src/main/java/tech/onlycoders/backend/repository/TagRepository.java
+++ src/main/java/tech/onlycoders/backend/repository/TagRepository.java
@@ -1,5 +1,6 @@
 package tech.onlycoders.backend.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,4 +17,27 @@ public interface TagRepository extends Neo4jRepository<Tag, String> {
 
   @Query("MATCH (t:Tag{canonicalName:$canonicalName})<-[:IS_INTERESTED]-(p:Person) RETURN count(p)")
   Long getFollowerQuantity(String canonicalName);
+
+  @Query("MATCH (t:Tag) WHERE t.canonicalName =~ $likeName RETURN count(t)")
+  int getTagQuantityByName(String likeName);
+
+  @Query(
+    "CALL {MATCH (t:Tag)<-[:IS_INTERESTED]-(p:Person) WHERE t.canonicalName =~ $likeName " +
+    "RETURN t, count(p) as quantity UNION " +
+    "MATCH (t:Tag) WHERE t.canonicalName =~ $likeName " +
+    "RETURN t, 0  as quantity} RETURN t ORDER BY quantity DESC SKIP $skip LIMIT $size "
+  )
+  List<Tag> getTagsByNamePaginated(String likeName, Integer skip, Integer size);
+
+  @Query("MATCH (t:Tag) RETURN count(t)")
+  int getTagQuantity();
+
+  @Query(
+    "CALL { " +
+    "MATCH (t:Tag)<-[:IS_INTERESTED]-(p:Person) RETURN t, count(p) as quantity " +
+    "UNION " +
+    "MATCH (t:Tag) RETURN t, 0 as quantity} " +
+    "RETURN t ORDER BY quantity DESC SKIP $skip LIMIT $size"
+  )
+  List<Tag> getTagsPaginated(Integer skip, Integer size);
 }
