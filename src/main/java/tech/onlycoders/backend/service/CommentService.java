74e79fe74ae2f7bd72e873561899a142babb63fa
Mariano Zunino <6627528+marianozunino@users.noreply.github.com>
2021-05-29 21:15:03 -0300

    Feature/add remove commen reaction (#49)
    * test: add/remove comment reactions
    
    * feat: add/remove comment reactions

---
+++ src/main/java/tech/onlycoders/backend/service/CommentService.java
@@ -0,0 +1,55 @@
+package tech.onlycoders.backend.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import tech.onlycoders.backend.dto.post.request.CreateReactionDto;
+import tech.onlycoders.backend.exception.ApiException;
+import tech.onlycoders.backend.model.Reaction;
+import tech.onlycoders.backend.repository.CommentRepository;
+import tech.onlycoders.backend.repository.ReactionRepository;
+import tech.onlycoders.backend.repository.UserRepository;
+
+@Service
+@Transactional
+public class CommentService {
+
+  private final CommentRepository commentRepository;
+  private final UserRepository userRepository;
+  private final ReactionRepository reactionRepository;
+
+  public CommentService(
+    CommentRepository commentRepository,
+    UserRepository userRepository,
+    ReactionRepository reactionRepository
+  ) {
+    this.commentRepository = commentRepository;
+    this.userRepository = userRepository;
+    this.reactionRepository = reactionRepository;
+  }
+
+  public void reactToComment(String canonicalName, String commentId, CreateReactionDto createReactionDto)
+    throws ApiException {
+    var comment =
+      this.commentRepository.findById(commentId)
+        .orElseThrow(() -> new ApiException(HttpStatus.NOT_FOUND, "error.comment-not-found"));
+    var user =
+      this.userRepository.findByCanonicalName(canonicalName)
+        .orElseThrow(() -> new ApiException(HttpStatus.INTERNAL_SERVER_ERROR, "error.500"));
+    this.reactionRepository.getCommentUserReaction(canonicalName, comment.getId())
+      .ifPresentOrElse(
+        reaction -> {
+          this.reactionRepository.updateReaction(reaction.getId(), createReactionDto.getReactionType());
+        },
+        () -> {
+          var reaction = Reaction.builder().person(user).type(createReactionDto.getReactionType()).build();
+          this.reactionRepository.save(reaction);
+          this.reactionRepository.linkWithComment(reaction.getId(), comment.getId());
+        }
+      );
+  }
+
+  public void deleteCommentReaction(String canonicalName, String commentId) {
+    this.reactionRepository.removeCommentReaction(canonicalName, commentId);
+  }
+}
---
+++ src/test/java/tech/onlycoders/backend/service/CommentServiceTest.java
@@ -0,0 +1,110 @@
+package tech.onlycoders.backend.service;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Optional;
+import org.jeasy.random.EasyRandom;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tech.onlycoders.backend.dto.post.request.CreateReactionDto;
+import tech.onlycoders.backend.exception.ApiException;
+import tech.onlycoders.backend.model.Comment;
+import tech.onlycoders.backend.model.Post;
+import tech.onlycoders.backend.model.Reaction;
+import tech.onlycoders.backend.model.User;
+import tech.onlycoders.backend.repository.CommentRepository;
+import tech.onlycoders.backend.repository.ReactionRepository;
+import tech.onlycoders.backend.repository.UserRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class CommentServiceTest {
+
+  @InjectMocks
+  private CommentService service;
+
+  @Mock
+  private UserRepository userRepository;
+
+  @Mock
+  private ReactionRepository reactionRepository;
+
+  @Mock
+  private CommentRepository commentRepository;
+
+  private final EasyRandom ezRandom = new EasyRandom();
+
+  @Test
+  public void ShouldReactToPost() throws ApiException {
+    var comment = ezRandom.nextObject(Comment.class);
+    var user = ezRandom.nextObject(User.class);
+    var createReactionDto = ezRandom.nextObject(CreateReactionDto.class);
+
+    Mockito.when(commentRepository.findById(comment.getId())).thenReturn(Optional.of(comment));
+    Mockito.when(userRepository.findByCanonicalName(user.getCanonicalName())).thenReturn(Optional.of(user));
+
+    Mockito
+      .when(reactionRepository.getCommentUserReaction(user.getCanonicalName(), comment.getId()))
+      .thenReturn(Optional.empty());
+
+    service.reactToComment(user.getCanonicalName(), comment.getId(), createReactionDto);
+  }
+
+  @Test
+  public void ShouldUpdatePostReaction() throws ApiException {
+    var reaction = ezRandom.nextObject(Reaction.class);
+    var comment = ezRandom.nextObject(Comment.class);
+    var user = ezRandom.nextObject(User.class);
+    var createReactionDto = ezRandom.nextObject(CreateReactionDto.class);
+
+    Mockito.when(commentRepository.findById(comment.getId())).thenReturn(Optional.of(comment));
+    Mockito.when(userRepository.findByCanonicalName(user.getCanonicalName())).thenReturn(Optional.of(user));
+
+    Mockito
+      .when(reactionRepository.getCommentUserReaction(user.getCanonicalName(), comment.getId()))
+      .thenReturn(Optional.of(reaction));
+
+    service.reactToComment(user.getCanonicalName(), comment.getId(), createReactionDto);
+  }
+
+  @Test
+  public void ShouldFailToReactToPostPostNotFound() {
+    var comment = ezRandom.nextObject(Comment.class);
+    var user = ezRandom.nextObject(User.class);
+    var createReactionDto = ezRandom.nextObject(CreateReactionDto.class);
+
+    Mockito.when(commentRepository.findById(comment.getId())).thenReturn(Optional.empty());
+
+    assertThrows(
+      Exception.class,
+      () -> service.reactToComment(user.getCanonicalName(), comment.getId(), createReactionDto)
+    );
+  }
+
+  @Test
+  public void ShouldFailToReactToPostUserNotFound() {
+    var comment = ezRandom.nextObject(Comment.class);
+    var user = ezRandom.nextObject(User.class);
+    var createReactionDto = ezRandom.nextObject(CreateReactionDto.class);
+
+    Mockito.when(commentRepository.findById(comment.getId())).thenReturn(Optional.of(comment));
+    Mockito.when(userRepository.findByCanonicalName(user.getCanonicalName())).thenReturn(Optional.empty());
+
+    assertThrows(
+      Exception.class,
+      () -> service.reactToComment(user.getCanonicalName(), comment.getId(), createReactionDto)
+    );
+  }
+
+  @Test
+  public void ShouldDeleteCommentReaction() {
+    var post = ezRandom.nextObject(Post.class);
+    var user = ezRandom.nextObject(User.class);
+    Mockito.doNothing().when(reactionRepository).removeCommentReaction(user.getCanonicalName(), post.getId());
+
+    service.deleteCommentReaction(user.getCanonicalName(), post.getId());
+  }
+}
