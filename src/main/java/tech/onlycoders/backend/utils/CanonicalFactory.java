66ec16ca2efeb013ab8c39c955d789af508e19ac
Mariano Z <marianoz@zoho.com>
2021-06-10 10:59:15 -0300

    feat: allow +-_ in cn

--- src/main/java/tech/onlycoders/backend/utils/CanonicalFactory.java
+++ src/main/java/tech/onlycoders/backend/utils/CanonicalFactory.java
@@ -5,6 +5,6 @@ import java.text.Normalizer;
 public class CanonicalFactory {
 
   public static String getCanonicalName(String name) {
-    return Normalizer.normalize(name, Normalizer.Form.NFD).replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+    return Normalizer.normalize(name, Normalizer.Form.NFD).replaceAll("[^a-zA-Z0-9-_+]", "").toLowerCase();
   }
 }
