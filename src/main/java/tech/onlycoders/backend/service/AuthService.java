548588a8c4a959b679ffeeb504c43a1f7de0dfe6
Martin Navarrete <martin.navarrete@mercadolibre.com>
2021-06-10 22:36:40 -0300

    fix: auth service

--- src/main/java/tech/onlycoders/backend/service/AuthService.java
+++ src/main/java/tech/onlycoders/backend/service/AuthService.java
@@ -69,7 +69,7 @@ public class AuthService {
       var user = optionalUser.get();
       extendClaims(user, claims);
       claims.put("eliminationDate", user.getEliminationDate().orElse(null));
-      claims.put("language", user.getLanguage().orElse(null));
+      claims.put("language", (user.getLanguage().isPresent() ? user.getLanguage().get().getCode() : null));
       this.workPositionRepository.getUserCurrentPosition(user.getCanonicalName())
         .ifPresentOrElse(
           workPosition ->
@@ -112,7 +112,7 @@ public class AuthService {
     claims.put("imageURI", person.getImageURI());
     claims.put("fullName", person.getFirstName() + " " + person.getLastName());
     claims.put("defaultPrivacy", person.getDefaultPrivacyIsPublic());
-    claims.put("language", person.getLanguage());
+    claims.put("language", (person.getLanguage() != null ? person.getLanguage().getCode() : null));
     var newToken = this.jwtService.createToken(claims, person.getEmail());
     return AuthResponseDto.builder().token(newToken).build();
   }
