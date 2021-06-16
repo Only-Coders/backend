f30bb30a1414462b8c42d99d2842d9bdda04616b
Martin Navarrete <martin.navarrete@mercadolibre.com>
2021-06-15 22:47:44 -0300

    fix: query

--- src/main/java/tech/onlycoders/backend/repository/GenericRepository.java
+++ src/main/java/tech/onlycoders/backend/repository/GenericRepository.java
@@ -1,7 +1,9 @@
 package tech.onlycoders.backend.repository;
 
 import java.util.Collection;
+import java.util.Date;
 import java.util.Map;
+import org.joda.time.DateTime;
 import org.springframework.data.neo4j.core.Neo4jClient;
 import org.springframework.stereotype.Repository;
 
@@ -15,9 +17,14 @@ public class GenericRepository {
   }
 
   public Collection<Map<String, Object>> getPostsPerDay() {
+    var date = new DateTime(new Date());
+    date = date.minusMonths(3);
+
     var query =
       "MATCH (p:Post)\n" +
-      "WHERE p.createdAt > 1623713625000\n" +
+      "WHERE p.createdAt > " +
+      date.toInstant().getMillis() +
+      "\n" +
       "WITH apoc.date.format(p.createdAt, \"ms\", \"yyyy-MM-dd\") AS date,\n" +
       "COUNT(p) as found\n" +
       "RETURN date, found";
